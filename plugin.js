e895df4d5ee22e1f48f05937566a7699afe217bb
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-05-18 17:58:34 -0300

    remove a necessidade de ativar o plugin pelo menu de workspace

--- plugin.js
+++ plugin.js
@@ -1,44 +1,24 @@
-const pluginState = {
-  active: false,
-};
-
 module.exports.responseHooks = [
   ({ response }) => {
-    if (pluginState.active) {
-      const bodyXml = response.getBody().toString();
-      const textJson = getContent(bodyXml);
+    const responseBody = response.getBody().toString();
+    const textJson = getContent(responseBody);
+
+    if (textJson) {
       const buffer = Buffer.from(textJson);
       response.setBody(buffer);
     }
   },
 ];
 
-module.exports.workspaceActions = [
-  {
-    label: "XML-JSON",
-    action: ({ app }) => {
-      pluginState.active = !pluginState.active;
-
-      app.alert(
-        "XML-JSON",
-        pluginState.active ? "Plugin activated" : "Plugin inactived"
-      );
-    },
-  },
-];
-
-function getContent(xml) {
-  const parser = new DOMParser();
-
-  const doc = parser.parseFromString(xml, "text/xml");
-
-  const strings = doc.getElementsByTagName("string");
-
-  if (strings.length > 0) {
-    let textContent = strings.item(0).firstChild.textContent;
+function getContent(responseBody) {
+  try {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(responseBody, "text/xml");
+    const strings = doc.getElementsByTagName("string");
+    const textContent = strings.item(0).firstChild.textContent;
 
     return textContent;
+  } catch {
+    return null;
   }
-
-  return xml;
 }
